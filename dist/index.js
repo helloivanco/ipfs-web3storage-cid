ad10b687667e779d4d72dd30be892faf0ff96709
Ivan <hellopaidco@gmail.com>
2023-03-03 01:28:51 +0000

    update

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+import { packToBlob } from 'ipfs-car/pack/blob';
+export const getCID = async (buffer) => {
+    const { root } = await packToBlob({
+        input: [buffer],
+        wrapWithDirectory: false,
+        maxChunkSize: 1048576,
+        maxChildrenPerNode: 1024,
+    });
+    return root.toString();
+};
